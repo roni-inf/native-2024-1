562e0c90d51a1f14ae89515d0771488cefe87f1c
roni-inf <roni_inf@hotmail.com>
2024-06-19 21:50:20 -0300

    Install AsyncStorage

--- aula-animation/App.tsx
+++ aula-animation/App.tsx
@@ -1,45 +1,82 @@
-import { View, Text, Animated, StyleSheet } from "react-native";
+import { View, Text, Animated, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useRef, useEffect } from "react";
+import * as Animatable from 'react-native-animatable';
 
 export default function App() {
-  const larguraAnimada = useRef(new Animated.Value(100)).current;
-  const alturaAnimada = useRef(new Animated.Value(150)).current;
-  const opacidade = useRef(new Animated.Value(1)).current;
+  const larguraAnimada = useRef(new Animated.Value(0)).current;
+  const alturaAnimada = useRef(new Animated.Value(50)).current;
+
+  const ButtonAnimaTable = Animatable.createAnimatableComponent(TouchableOpacity);
+
+  //const opacidade = useRef(new Animated.Value(1)).current;
+
+  //   useEffect(() => {
+
+  //     Animated.loop(
+  //     Animated.sequence([
+  //       Animated.timing(larguraAnimada, {
+  //         toValue: 300,
+  //         duration: 4000,
+  //         useNativeDriver: false,
+  //       }),
+
+  //       Animated.timing(alturaAnimada, {
+  //         toValue: 300,
+  //         duration: 4000,
+  //         useNativeDriver: false,
+  //       }),
+
+  //     //   Animated.timing(opacidade, {
+  //     //     toValue: 0,
+  //     //     duration: 2000,
+  //     //     useNativeDriver: false,
+  //     //   }),
+  //     ])
+
+  //     ).start();
+  //   }, []);
 
   useEffect(() => {
     Animated.sequence([
       Animated.timing(larguraAnimada, {
-        toValue: 300,
-        duration: 4000,
+        toValue: 100,
+        duration: 3000,
         useNativeDriver: false,
       }),
-
       Animated.timing(alturaAnimada, {
-        toValue: 300,
-        duration: 4000,
-        useNativeDriver: false,
-      }),
-
-      Animated.timing(opacidade, {
-        toValue: 0,
-        duration: 2000,
+        toValue: 100,
+        duration: 3000,
         useNativeDriver: false,
       }),
     ]).start();
   }, []);
 
+  let porcentagemLargura = larguraAnimada.interpolate({
+    inputRange: [50, 100],
+    outputRange: ["5%", "100%"],
+  });
+
+  let porcentagemAltura = alturaAnimada.interpolate({
+    inputRange: [50, 100],
+    outputRange: ["0%", "100%"],
+  });
+
   return (
     <View style={styles.container}>
       <Animated.View
         style={{
-          width: larguraAnimada,
-          height: alturaAnimada,
+          width: porcentagemLargura,
+          height: porcentagemAltura,
           backgroundColor: "#4169e1",
           justifyContent: "center",
-          opacity: opacidade,
+          
+          //  opacity: opacidade,
         }}
       >
-        <Text style={styles.title}>Loading......</Text>
+        <Animatable.Text style={styles.title} animation='shake' duration={5000}>Loading......</Animatable.Text>
+        <ButtonAnimaTable animation='shake' style={styles.botao}>
+            <Text>Botão Animado</Text>
+         </ButtonAnimaTable>
       </Animated.View>
     </View>
   );
@@ -56,4 +93,12 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: "#fff",
   },
+  botao:{
+    width:'100%',
+    height:30,
+    color:'#fff',
+    backgroundColor:'green',
+    alignItems:'center',
+    justifyContent:'center'
+  }
 });
